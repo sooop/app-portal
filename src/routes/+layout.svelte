05a2e6f94fb625f745646ddce66aeb53d950ecd6
sooop <soooprmx@gmail.com>
2025-09-03 19:39:28 +0900

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<div class="min-h-screen w-full bg-gradient-to-br from-blue-400 to-purple-500">
+{@render children?.()}
+</div>
