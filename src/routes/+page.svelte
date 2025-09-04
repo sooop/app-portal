43bd78961b9845dc8d64bd09d5ff3bcfd3724c1f
sooop <soooprmx@gmail.com>
2025-09-04 16:42:07 +0900

    change each link into component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,27 @@
+<script>
+	import Link from './Link.svelte';
+	const links = [
+		{ 'link':'https://claude.ai/public/artifacts/87a7cbc4-067c-4477-b3f7-99b0be7f9894',
+			'title':'설문응답 분석하기'},
+		{'link':'https://claude.ai/public/artifacts/d37c4bc8-d607-4a92-a885-6dc1026a2ba6',
+			'title':'수강생 정보 분석'}
+		];
+
+</script>
 <div
-  class={[
-    "flex flex-row items-center justify-center w-full h-full",
-    "min-h-screen",
-  ]}
+	class={[
+		"flex flex-row items-center justify-center w-full h-full",
+		"min-h-screen",
+	]}
 >
-  <div class="flex flex-col">
-    <h1 class="text-xl font-bold text-white">List of Applications</h1>
-    <div class="p-4">
-      <ul>
-        <li class="mb-4 text-sm text-white mx-1">
-          <a
-            class="inline-block rounded-sm p-2 hover:bg-blue-600 alpha-90 transition-color duration-200"
-            href="https://claude.ai/public/artifacts/87a7cbc4-067c-4477-b3f7-99b0be7f9894"
-            target="_blank">설문응답분석하기</a
-          >
-        </li>
-        <li class="mb-4 text-sm text-white mx-1">
-          <a
-            class="inline-block rounded-sm p-2 hover:bg-blue-600 alpha-90 transition-color duration-200"
-            href="https://claude.ai/public/artifacts/d37c4bc8-d607-4a92-a885-6dc1026a2ba6"
-            target="_blank">수강생 정보 분석</a
-          >
-        </li>
-      </ul>
-    </div>
-  </div>
+	<div class="flex flex-col">
+		<h1 class="text-xl font-bold text-white">List of Applications</h1>
+		<div class="p-4">
+			<ul>
+				{#each links as {link, title}}
+					<Link {link} {title}/>
+				{/each}
+			</ul>
+		</div>
+	</div>
 </div>
---
+++ src/routes/Link.svelte
@@ -0,0 +1,11 @@
+<script>
+	const {title="Untitled Link", link} = $props();
+</script>
+
+<li class="mb-4 text-sm text-white mx-1">
+	<a href={link} target="_blank"
+		class={["inline-block rounded-sm p-2",
+	"hover:bg-blue-600 transition-colur duration-200"]}
+		>{title}</a>
+
+</li>
